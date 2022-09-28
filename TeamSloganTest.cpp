c6aba01bbe3d0bcc1c513ab157e2e4a771c4f774
TUTICOI <gdkhanhnguyen2305@gmail.com>
2022-09-28 08:22:58 +0700

    Added class human with eat, code, sleep function.

--- TeamSloganTest.cpp
+++ TeamSloganTest.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 using namespace std;
 
+class human
+{
+protected:
+    bool alive = 1;
+
+public:
+    human()
+    {
+        alive = 1;
+    }
+    void eat()
+    {
+        cout << "Eating." << endl;
+    }
+    void code()
+    {
+        cout << "Coding." << endl;
+    }
+    void sleep()
+    {
+        cout << "Sleeping." << endl;
+    }
+};
+
 int main()
 {
-    // TeamSloganTestByTUTICOI
+
     return 0;
 }
