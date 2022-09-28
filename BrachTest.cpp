7239d10c2bbbf3b991aed0edebac5d66d60d5fa3
haviethoang1904 <hoanghadn300@gmail.com>
2022-09-28 21:16:15 +0700

    branch test

---
+++ BrachTest.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "git branch <name new branch";
+    cout << "git checkout <name new branch";
+    cout << "git checkout -b >name new branch";
+    return 0;
+}
